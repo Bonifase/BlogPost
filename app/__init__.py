599ff23eaa5e8f9c7bc91e01d83c9a343edba7f0
Bonifase Orwa <orwabonifase@gmail.com>
2018-07-18 17:53:42 +0300

    [feature] add views file

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+from config import app_config
+
+db = SQLAlchemy()
+app = Flask(__name__, instance_relative_config=True)
+
+from app import views
+
+app.config['JWT_SECRET_KEY'] = 'supersecretishere'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config.from_object(app_config["development"])
+
+db.init_app(app)
+CORS(app)
---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+# from .post import Post
+# from .comment import comment
+# from .like import Like
