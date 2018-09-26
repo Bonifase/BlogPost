8ababa18108d15b386c1e4faf8e98a525538ca46
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-26 19:55:23 +0300

    add data validation

--- blogpost/forms/forms.py
+++ blogpost/forms/forms.py
@@ -1,7 +1,8 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from blogpost.models.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +14,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField(
         'Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                'That username is already taken. Please use a diffrent one')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is already taken. Please use a diffrent one')
 
 
 class LoginForm(FlaskForm):
