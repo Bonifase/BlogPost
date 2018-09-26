a58cc83dc94a7e9be72be742f52857f50125513e
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-26 10:24:33 +0300

    add basic routes

--- blog_post.py
+++ blog_post.py
@@ -3,8 +3,14 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+@app.route("/home")
+def home():
     return "<h1>Home Page</h1>"
+
+
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
 
 
 if __name__ == '__main__':
