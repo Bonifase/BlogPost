e63f9afa37c575fc5001c2ad141e9cf566af4ea7
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-26 14:10:29 +0300

    add user login route

--- blog_post.py
+++ blog_post.py
@@ -20,7 +20,9 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created successfully for {form.username.data} !')
+        flash(
+            f'Account created successfully for {form.username.data} !',
+            'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
