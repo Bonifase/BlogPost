89528f660ed8658f4ce4bd53d01ee5aedd95c039
Bonifase Orwa <orwabonifase@gmail.com>
2018-09-27 01:09:21 +0300

    User Account and Profile Picture

--- blogpost/forms/forms.py
+++ blogpost/forms/forms.py
@@ -1,5 +1,7 @@
 
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError  # noqa
 from blogpost.models.models import User
@@ -34,3 +36,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField(
+        'Upload Profile Picture', validators=[FileAllowed(
+            ['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'That username is already taken. Please use a diffrent one')  # noqa
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'That email is already taken. Please use a diffrent one')
